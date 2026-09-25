d1c25101115e01b97189b840251d671eda4b04c3
seteiro <276597748+cms19859230182-lang@users.noreply.github.com>
2026-09-25 14:58:12 +0800

    [bsp][esp32-c6] declare rtc_gpio_force_hold_en_all before use

--- components/driver/gpio/rtc_io.c
+++ components/driver/gpio/rtc_io.c
@@ -177,6 +177,8 @@ esp_err_t rtc_gpio_isolate(gpio_num_t gpio_num)
     return ESP_OK;
 }
 
+esp_err_t rtc_gpio_force_hold_en_all(void);
+
 esp_err_t rtc_gpio_force_hold_all(void)
 {
     return rtc_gpio_force_hold_en_all();
